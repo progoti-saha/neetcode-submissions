dde05a5e1ec860a8d4fb954f6636c7d1bc3916f5
Progoti Saha <progotisaha65@gmail.com>
2026-09-07 17:50:34 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    boolean isMatching(char x,char y){
+       if((x=='(' && y==')') || 
+       (x=='{' && y=='}') || 
+       (x=='[' && y==']')) {
+        return true;
+       }
+       return false;
+       }
+
+    public boolean isValid(String s) {
+        Stack<Character> myStack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{'|| s.charAt(i)=='['){
+                myStack.push(s.charAt(i));
+            }
+            else{
+            if(myStack.isEmpty() == true)
+            {
+               return false;
+            }
+            else if(isMatching(myStack.peek(),s.charAt(i))==false)
+            return false; 
+            else{
+                myStack.pop();
+            } 
+        }
+        }  
+        return myStack.isEmpty();  
+    }
+}
